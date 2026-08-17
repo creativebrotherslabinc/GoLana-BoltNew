960dd6e128ddbaaee0231e9a0f8a06543239e6a2
creativebrotherslabinc <creativebrotherslabinc@gmail.com>
2026-08-17 18:55:42 -0400

    version 4

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11595909
+    "zoneId": 11599149
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
